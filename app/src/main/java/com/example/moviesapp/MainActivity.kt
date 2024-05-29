e1a5e7a4651e5daa90c4345bbcc9f9e2a88bc300
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2024-05-29 23:09:28 +0530

    feat: added lazy column

--- app/src/main/java/com/example/moviesapp/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/MainActivity.kt
@@ -5,15 +5,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.moviesapp.components.MovieRow
 import com.example.moviesapp.ui.theme.MoviesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,15 +43,42 @@ fun MyApp(content: @Composable () -> Unit){
         Scaffold(
             topBar = {
                 TopAppBar(title = {
-                    Text(text = "MoviesApp")
+                    Text(text = "MoviesApp", textAlign = TextAlign.Center)
                 })
             }
         ) {
-            content()
+            innerPadding -> Box(
+                modifier = Modifier.padding(innerPadding)
+            ){
+                content()
+            }
         }
     }
 }
 @Composable
-fun MainContent(){
-    Text(text = "Hello")
+fun MainContent(movieList: List<String> = listOf(
+    "Avatar",
+    "300",
+    "Harry Potter",
+    "Life",
+    "Ghost of Tsushima",
+    "Boku No Hero Academia",
+    "Shingeki No Kyojin",
+)){
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(items = movieList){
+                MovieRow(movie = it)
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun MyAppPreview(){
+    MyApp {
+        MainContent()
+    }
 }
