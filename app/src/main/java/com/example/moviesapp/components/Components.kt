81102160f162373468553929f364acaa9060b5db
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2024-06-02 00:12:21 +0530

    feat: added coil image handler

--- app/src/main/java/com/example/moviesapp/components/Components.kt
+++ app/src/main/java/com/example/moviesapp/components/Components.kt
@@ -1,7 +1,9 @@
 package com.example.moviesapp.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,23 +16,31 @@ import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.ImageLoader
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.example.moviesapp.model.Movie
+import com.example.moviesapp.model.getMovies
 
+@Preview(showBackground = true)
 @Composable
-fun MovieRow(movie:Movie, onItemClick: (String) -> Unit = {}){
+fun MovieRow(movie:Movie = getMovies()[0], onItemClick: (String) -> Unit = {}){
     Card(modifier = Modifier
         .padding(4.dp)
         .fillMaxWidth()
         .height(130.dp)
         .clickable {
-                   onItemClick(movie.id)
+            onItemClick(movie.id)
         }
         ,
         shape = RoundedCornerShape(corner = CornerSize(16.dp)),
@@ -45,10 +55,23 @@ fun MovieRow(movie:Movie, onItemClick: (String) -> Unit = {}){
                 shape = RectangleShape,
                 tonalElevation = 4.dp
             ) {
-                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image" )
+                Image(
+                    painter = rememberAsyncImagePainter(
+                        model = ImageRequest.Builder(LocalContext.current).data(movie.images[0]).crossfade(true).build(),
+
+                   ), contentDescription = "Movie Image")
 
             }
-            Text(text = movie.title);
+            Column(
+                modifier = Modifier.padding(4.dp)
+            ) {
+                Text(text = movie.title,
+                    style = MaterialTheme.typography.headlineSmall
+                );
+                Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.labelSmall);
+                Text(text = "Released: ${movie.year}",style = MaterialTheme.typography.labelSmall);
+            }
+
         }
     }
 }
